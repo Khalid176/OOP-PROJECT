6048baa27aca5ae047fd6bc33540d68a1db5927a
Khalid176 <muhammad.khalid2006@gmail.com>
2026-04-27 19:37:42 +0500

    fixed God forsaken bugs :-1:

--- project.cpp
+++ project.cpp
@@ -1086,72 +1086,216 @@ public:
         ofstream file_temp("Temp.txt");
         if (!(file_real.is_open() && file_temp.is_open()))
         {
-            cout << "UNABLE TO OPEN THE FILE " << endl;
+            cout << "UNABLE TO OpEN THE FILE " << endl;
             return 0;
         }
         else
         {
             string data;
+            int k = 0;
 
             while (getline(file_real, data))
             {
                 int i = 0;
 
-                string Picked_password;
-                string Picked_cnic;
-                string Picked_name;
-                string Picked_Gender;
-                string Picked_phone;
-                string Picked_city;
-                bool Picked_is_blocked;
+                string picked_password;
+                string picked_cnic;
+                string picked_name;
+                string picked_Gender;
+                string picked_phone;
+                string picked_city;
+                bool picked_is_blocked;
 
-                for (; data[i] = '|'; i++)
+                for (; data[i] != '|'; i++)
                 {
-                    Picked_cnic = Picked_cnic + data[i];
+                    picked_cnic = picked_cnic + data[i];
                 }
-                if (Picked_cnic != cnic)
+                i++;
+
+                if (picked_cnic != cnic)
                 {
+
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_name += data[i];
+                        picked_password += data[i];
                     }
 
                     i++;
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_Gender += data[i];
+                        picked_name += data[i];
                     }
 
                     i++;
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_phone += data[i];
+                        picked_Gender += data[i];
                     }
 
                     i++;
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_city += data[i];
+                        picked_phone += data[i];
+                    }
+
+                    i++;
+                    for (; data[i] != '|'; i++)
+                    {
+                        picked_city += data[i];
                     }
 
                     i++;
                     for (; data[i] != '\0'; i++)
                     {
-                        Picked_is_blocked = (data[i] == '1');
+                        picked_is_blocked = (data[i] == '1');
                     }
-                    file_temp << Picked_cnic << "|" << Picked_password << "|" << Picked_name << "|" << Picked_Gender << "|" << Picked_phone << "|" << Picked_city << "|" << Picked_is_blocked << endl;
+
+                    file_temp << picked_cnic << "|" << picked_password << "|" << picked_name << "|" << picked_Gender << "|" << picked_phone << "|" << picked_city << "|" << picked_is_blocked << endl;
                 }
             }
+            file_real.close();
+            file_temp.close();
             ofstream file_real("Customers.txt");
             ifstream file_temp("Temp.txt");
             if (!(file_real.is_open() && file_temp.is_open()))
             {
+            }
+            else
+            {
                 while (getline(file_temp, data))
                 {
-                    file_real << data << endl;
+                    int i = 0;
+                    string picked_password;
+                    string picked_cnic;
+                    string picked_name;
+                    string picked_Gender;
+                    string picked_phone;
+                    string picked_city;
+                    bool picked_is_blocked;
+
+                    for (; data[i] != '|'; i++)
+                    {
+                        picked_cnic = picked_cnic + data[i];
+                    }
+                    i++;
+
+                    if (picked_cnic != cnic)
+                    {
+                        for (; data[i] != '|'; i++)
+                        {
+                            picked_password += data[i];
+                        }
+                        i++;
+                        for (; data[i] != '|'; i++)
+                        {
+                            picked_name += data[i];
+                        }
+
+                        i++;
+                        for (; data[i] != '|'; i++)
+                        {
+                            picked_Gender += data[i];
+                        }
+
+                        i++;
+                        for (; data[i] != '|'; i++)
+                        {
+                            picked_phone += data[i];
+                        }
+
+                        i++;
+                        for (; data[i] != '|'; i++)
+                        {
+                            picked_city += data[i];
+                        }
+
+                        i++;
+                        for (; data[i] != '\0'; i++)
+                        {
+                            picked_is_blocked = (data[i] == '1');
+                        }
+                        file_real << picked_cnic << "|" << picked_password << "|" << picked_name << "|" << picked_Gender << "|" << picked_phone << "|" << picked_city << "|" << picked_is_blocked << endl;
+                    }
+                }
+                file_real.close();
+                file_temp.close();
+            }
+
+            ifstream file_real2("Sessions.txt");
+            ofstream file_temp2("Temp2.txt");
+            if (!(file_real2.is_open() && file_temp2.is_open()))
+            {
+                cout << "UNABLE TO OpEN THE FILE second last" << endl;
+                return 0;
+            }
+            else
+            {
+                cout << "ENter";
+                string data2;
+                if (!(file_real2.is_open() && file_temp2.is_open()))
+                {
+                    return 0;
+                }
+                else
+                {
+                    while (getline(file_real2, data2))
+                    {
+                        int i = 0;
+                        string picked_cnic, picked_Timestamp, picked_FiltersAplied, picked_OutputFile;
+                        for (; data2[i] != '|'; i++)
+                        {
+                            picked_cnic = picked_cnic + data2[i];
+                        }
+                        i++;
+                        if (picked_cnic != cnic)
+                        {
+                            for (; data2[i] != '|'; i++)
+                            {
+                                picked_Timestamp += data2[i];
+                            }
+
+                            i++;
+                            for (; data2[i] != '|'; i++)
+                            {
+                                picked_FiltersAplied += data2[i];
+                            }
+
+                            i++;
+                            for (; data2[i] != '\0'; i++)
+                            {
+                                picked_OutputFile += data2[i];
+                            }
+
+                            i++;
+
+                            file_temp2 << picked_cnic << "|" << picked_Timestamp << "|" << picked_FiltersAplied << "|" << picked_OutputFile << endl;
+                        }
+                    }
+
+                    file_real2.close();
+                    file_temp2.close();
+                    ofstream file_real2("Sessions.txt");
+                    ifstream file_temp2("Temp2.txt");
+
+                    if (!(file_real2.is_open() && file_temp2.is_open()))
+                    {
+                        cout << "UNABLE TO OPEN THE FILE last" << endl;
+                        return 0;
+                    }
+                    else
+                    {
+                        while (getline(file_temp2, data2))
+                        {
+                            cout << "Copying";
+                            file_real2 << data2 << endl;
+                        }
+                    }
+
+                    file_real2.close();
+                    file_temp2.close();
+                    return 1;
                 }
             }
-            return 1;
         }
     }
 };
--- test3.cpp
+++ test3.cpp
@@ -4,99 +4,227 @@
 #include <string>
 using namespace std;
 bool F_M_Delete(string cnic)
+{
+    int count = 5;
+    ifstream file_real("Customers.txt");
+    ofstream file_temp("Temp.txt");
+    if (!(file_real.is_open() && file_temp.is_open()))
     {
-        int count = 5;
-        ifstream file_real("Customers.txt");
-        ofstream file_temp("Temp.txt");
-        if (!(file_real.is_open() && file_temp.is_open()))
-        {
-            cout << "UNABLE TO OPEN THE FILE " << endl;
-            return 0;
-        }
-        else
-        {
-            string data;
-            int k = 0;
+        cout << "UNABLE TO OpEN THE FILE " << endl;
+        return 0;
+    }
+    else
+    {
+        string data;
+        int k = 0;
 
-            while (getline(file_real, data))
+        while (getline(file_real, data))
+        {
+            int i = 0;
+
+            string picked_password;
+            string picked_cnic;
+            string picked_name;
+            string picked_Gender;
+            string picked_phone;
+            string picked_city;
+            bool picked_is_blocked;
+
+            for (; data[i] != '|'; i++)
             {
-                int i = 0;
+                picked_cnic = picked_cnic + data[i];
+            }
+            i++;
 
-                string Picked_password;
-                string Picked_cnic;
-                string Picked_name;
-                string Picked_Gender;
-                string Picked_phone;
-                string Picked_city;
-                bool Picked_is_blocked;
+            if (picked_cnic != cnic)
+            {
 
                 for (; data[i] != '|'; i++)
                 {
-                    Picked_cnic = Picked_cnic + data[i];
+                    picked_password += data[i];
+                }
+
+                i++;
+                for (; data[i] != '|'; i++)
+                {
+                    picked_name += data[i];
+                }
+
+                i++;
+                for (; data[i] != '|'; i++)
+                {
+                    picked_Gender += data[i];
+                }
+
+                i++;
+                for (; data[i] != '|'; i++)
+                {
+                    picked_phone += data[i];
+                }
+
+                i++;
+                for (; data[i] != '|'; i++)
+                {
+                    picked_city += data[i];
+                }
+
+                i++;
+                for (; data[i] != '\0'; i++)
+                {
+                    picked_is_blocked = (data[i] == '1');
+                }
+
+                file_temp << picked_cnic << "|" << picked_password << "|" << picked_name << "|" << picked_Gender << "|" << picked_phone << "|" << picked_city << "|" << picked_is_blocked << endl;
+            }
+        }
+        file_real.close();
+        file_temp.close();
+        ofstream file_real("Customers.txt");
+        ifstream file_temp("Temp.txt");
+        if (!(file_real.is_open() && file_temp.is_open()))
+        {
+        }
+        else
+        {
+            while (getline(file_temp, data))
+            {
+                int i = 0;
+                string picked_password;
+                string picked_cnic;
+                string picked_name;
+                string picked_Gender;
+                string picked_phone;
+                string picked_city;
+                bool picked_is_blocked;
+
+                for (; data[i] != '|'; i++)
+                {
+                    picked_cnic = picked_cnic + data[i];
                 }
                 i++;
-                cout<<"Cnic picked"<<endl;
-                if (Picked_cnic != cnic)
+
+                if (picked_cnic != cnic)
                 {
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_name += data[i];
+                        picked_password += data[i];
                     }
-                cout<<"name picked"<<endl;
+                    i++;
+                    for (; data[i] != '|'; i++)
+                    {
+                        picked_name += data[i];
+                    }
 
                     i++;
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_Gender += data[i];
+                        picked_Gender += data[i];
                     }
-                cout<<"Gender picked"<<endl;
 
                     i++;
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_phone += data[i];
+                        picked_phone += data[i];
                     }
-                cout<<"Phone picked"<<endl;
 
                     i++;
                     for (; data[i] != '|'; i++)
                     {
-                        Picked_city += data[i];
+                        picked_city += data[i];
                     }
-                cout<<"City picked"<<endl;
 
                     i++;
                     for (; data[i] != '\0'; i++)
                     {
-                        Picked_is_blocked = (data[i] == '1');
+                        picked_is_blocked = (data[i] == '1');
                     }
-                cout<<"block picked"<<endl;
-
-                    file_temp << Picked_cnic << "|" << Picked_password << "|" << Picked_name << "|" << Picked_Gender << "|" << Picked_phone << "|" << Picked_city << "|" << Picked_is_blocked << endl;
+                    file_real << picked_cnic << "|" << picked_password << "|" << picked_name << "|" << picked_Gender << "|" << picked_phone << "|" << picked_city << "|" << picked_is_blocked << endl;
                 }
-                cout<<++k<<endl<<endl;
-                
             }
             file_real.close();
             file_temp.close();
-            cout<<"P1"<<endl;
-            ofstream file_real("Customers.txt");
-            ifstream file_temp("Temp.txt");
-            if (!(file_real.is_open() && file_temp.is_open()))
+        }
+
+        
+        ifstream file_real2("Sessions.txt");
+        ofstream file_temp2("Temp2.txt");
+        if (!(file_real2.is_open() && file_temp2.is_open()))
+        {
+            cout << "UNABLE TO OpEN THE FILE second last" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "ENter";
+            string data2;
+            if (!(file_real2.is_open() && file_temp2.is_open()))
             {
-                cout<<"Unable to open file";
+                return 0;
             }
             else
             {
-                while (getline(file_temp, data))
+                while (getline(file_real2, data2))
                 {
-                    file_real << data << endl;
+                    int i = 0;
+                    string picked_cnic, picked_Timestamp, picked_FiltersAplied, picked_OutputFile;
+                    for (; data2[i] != '|'; i++)
+                    {
+                        picked_cnic = picked_cnic + data2[i];
+                    }
+                    i++;
+                    if (picked_cnic != cnic)
+                    {
+                        for (; data2[i] != '|'; i++)
+                        {
+                            picked_Timestamp += data2[i];
+                        }
+
+                        i++;
+                        for (; data2[i] != '|'; i++)
+                        {
+                            picked_FiltersAplied += data2[i];
+                        }
+
+                        i++;
+                        for (; data2[i] != '\0'; i++)
+                        {
+                            picked_OutputFile += data2[i];
+                        }
+
+                        i++;
+
+                        file_temp2 << picked_cnic << "|" << picked_Timestamp << "|" << picked_FiltersAplied << "|" << picked_OutputFile << endl;
+                    }
                 }
+
+                file_real2.close();
+                file_temp2.close();
+                ofstream file_real2("Sessions.txt");
+                ifstream file_temp2("Temp2.txt");
+
+                if (!(file_real2.is_open() && file_temp2.is_open()))
+                {
+                    cout << "UNABLE TO OPEN THE FILE last" << endl;
+                    return 0;
+                }
+                else
+                {
+                    while (getline(file_temp2, data2))
+                    {
+                        cout << "Copying";
+                        file_real2 << data2 << endl;
+                    }
+                }
+
+                file_real2.close();
+                file_temp2.close();
+                return 1;
             }
-            return 1;
         }
     }
-int main() 
+}
+
+int main()
 {
     F_M_Delete("123");
 }
